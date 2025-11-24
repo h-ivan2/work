88498ce765967dc5acbc9b74633966f1cd0e3ef2
hirwaivan <hirwaivan884@gmail@example.com>
2025-11-24 19:59:25 +0200

    js files

---
+++ indexe.js
@@ -0,0 +1,15 @@
+function IVAN(days){
+switch(days){
+    case "Monday":
+    console.log("We are on monday")
+    break;
+    case "Tuesday":
+    console.log("We are Tuesday")
+    break;
+    case "Wednesday":
+    console.log("We are wednesday")
+    break;
+    case "default":
+        console.log("Invalid day we are")
+}
+}
