87241cc00043a35b286de7544c88d724134b0f91
h-ivan2 <hirwaivan884@gmail.com>
2026-02-25 21:49:45 +0200

    Some practice about javascripts

--- first2.js
+++ first2.js
@@ -1,5 +1,13 @@
-function subtraction(number1,number2)
- return number1-number2
-}
-let difference = subtraction(5,3)
-console.log("difference of 5 and 3 is "diference)
+const subtract = (a,b) => {
+
+    return a-b;
+};
+
+console.log(subtract(4,3));
+
+const greet = name => {
+
+    console.log("Hello " + name);
+};
+
+greet("Alex");
