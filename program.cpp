a43990502a0dc0c8261a6d087aa55ca5bd06e156
h-ivan2 <hirwaivan884@gmail.com>
2026-02-25 21:54:46 +0200

    Other cpp basics codes

---
+++ program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    //cout <<"Enter the number you want to find the factorial: ";
+    //cin >> x ;
+
+    string firstName="HIRWA";
+    string lastName="  IVAN" ;
+    string fullName= firstName.append(lastName);
+    cout << fullName ;
+
+    return 0;
+
+
+
+}
